5e2fd368a9b674664d18d56ae945e50fb3a99838
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-01-11 23:43:30 -0300

    update theme (error)

--- src/lib/theme/stitches.config.ts
+++ src/lib/theme/stitches.config.ts
@@ -51,12 +51,12 @@ export const {
       small: "12px 38px",
     },
 
-    media: {
-      sm: "(min-width: 576px)",
-      md: "(min-width: 768px)",
-      lg: "(min-width: 992px)",
-      xl: "(min-width: 1200px)",
-      "2xl": "(min-width: 1400px)",
-    },
+  },
+  media: {
+    sm: '(min-width: 576px)',
+    md: '(min-width: 768px)',
+    lg: '(min-width: 992px)',
+    xl: '(min-width: 1200px)',
+    "2xl": '(min-width: 1400px)',
   },
 });
